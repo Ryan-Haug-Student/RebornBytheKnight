2c005768d4362e37a877ed1f66f39e4a17998db0
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-02-03 13:04:10 -0500

    Added a attack indicator and hitbox

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    GameObject attackIndicator;
+    GameObject AHB; //AttackHitBox
 
     //public stats
     [Header("Player Stats")]
@@ -24,6 +26,12 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        attackIndicator = GameObject.Find("AttackIndicator");
+
+        //this must be active at start to find object
+        AHB = GameObject.Find("AttackHitBox");
+        AHB.SetActive(false);
     }
 
 
@@ -33,6 +41,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && canAttack)
             Attack();
+        
     }
 
     private void Move()
@@ -40,15 +49,14 @@ public class PlayerController : MonoBehaviour
         Vector2 futurePos = Vector2.zero;
 
         if (Input.GetAxisRaw("Horizontal") != 0)
-        {
-            futurePos += new Vector2(Input.GetAxisRaw("Horizontal") * Time.deltaTime, 0);
-        }
+            futurePos += new Vector2(Input.GetAxisRaw("Horizontal") * Time.deltaTime * 10, 0);
+        
         if (Input.GetAxisRaw("Vertical") != 0)
-        {
-            futurePos += new Vector2(0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
-        }
+            futurePos += new Vector2(0, Input.GetAxisRaw("Vertical") * Time.deltaTime * 10);
+        
 
         rb.AddForce(futurePos.normalized * speed);
+        DirectionControl(futurePos);
 
         futurePos = Vector2.zero;
     }
@@ -56,12 +64,30 @@ public class PlayerController : MonoBehaviour
     private void Attack()
     {
         canAttack = false;
+
+        //manage the attackhitbox rotation while attacking
+        if (moveDirection == MoveDirection.UPRIGHT || moveDirection == MoveDirection.DOWNLEFT)
+            AHB.transform.rotation = Quaternion.Euler(0, 0, -45);
+        else if (moveDirection == MoveDirection.UPLEFT || moveDirection == MoveDirection.DOWNRIGHT)
+            AHB.transform.rotation = Quaternion.Euler(0, 0, 45);
+        else if (moveDirection == MoveDirection.LEFT || moveDirection == MoveDirection.RIGHT)
+            AHB.transform.rotation = Quaternion.Euler(0, 0, 90);
+        else
+            AHB.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        AHB.SetActive(true);
         Debug.Log("attacked");
+        Invoke("RemoveHitbox", attckCooldown * .2f);
+        
         Invoke("ResetAttack", attckCooldown);
     }
     private void ResetAttack()
     {
         canAttack = true;
+    }
+    private void RemoveHitbox()
+    {
+        AHB.SetActive(false);
     }
 
     private void DirectionControl(Vector2 futurePos)
@@ -69,54 +95,54 @@ public class PlayerController : MonoBehaviour
         switch (futurePos)
         {
             case Vector2 position when position == new Vector2(0, 0):
-                moveDirection = MoveDirection.UP;
-                Debug.Log("STATIC");
+                moveDirection = MoveDirection.STATIC;
+                attackIndicator.transform.position = this.transform.position;
                 break;
 
             case Vector2 position when position.x == 0 && position.y > 0:
                 moveDirection = MoveDirection.UP;
-                Debug.Log("UP");
+                attackIndicator.transform.position = this.transform.position + new Vector3(0, 1);
                 break;
 
             case Vector2 position when position.x > 0 && position.y > 0:
                 moveDirection = MoveDirection.UPRIGHT;
-                Debug.Log("UPRIGHT");
+                attackIndicator.transform.position = this.transform.position + new Vector3(1, 1);
                 break;
 
             case Vector2 position when position.x > 0 && position.y == 0:
                 moveDirection = MoveDirection.RIGHT;
-                Debug.Log("RIGHT");
+                attackIndicator.transform.position = this.transform.position + new Vector3(1, 0);
                 break;
 
             case Vector2 position when position.x > 0 && position.y < 0:
                 moveDirection = MoveDirection.DOWNRIGHT;
-                Debug.Log("DOWNRIGHT");
+                attackIndicator.transform.position = this.transform.position + new Vector3(1, -1);
                 break;
 
             case Vector2 position when position.x == 0 && position.y < 0:
                 moveDirection = MoveDirection.DOWN;
-                Debug.Log("DOWN");
+                attackIndicator.transform.position = this.transform.position + new Vector3(0, -1);
                 break;
 
             case Vector2 position when position.x < 0 && position.y < 0:
                 moveDirection = MoveDirection.DOWNLEFT;
-                Debug.Log("DOWNLEFT");
+                attackIndicator.transform.position = this.transform.position + new Vector3(-1, -1);
                 break;
 
             case Vector2 position when position.x < 0 && position.y == 0:
                 moveDirection = MoveDirection.LEFT;
-                Debug.Log("LEFT");
+                attackIndicator.transform.position = this.transform.position + new Vector3(-1, 0);
                 break;
 
             case Vector2 position when position.x < 0 && position.y > 0:
                 moveDirection = MoveDirection.UPLEFT;
-                Debug.Log("UPLEFT");
+                attackIndicator.transform.position = this.transform.position + new Vector3(-1, 1);
                 break;
         }
     }
 
     public enum MoveDirection
-        {
+    {
         UP,
         UPRIGHT,
         RIGHT,
