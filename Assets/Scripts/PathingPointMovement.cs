7a7e02ae1a1966aae2ef225d496d26590ddbb056
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-02-04 10:19:32 -0500

    pathing points now move in and out as needed

--- Assets/Scripts/PathingPointMovement.cs
+++ Assets/Scripts/PathingPointMovement.cs
@@ -5,77 +5,52 @@ using UnityEngine;
 public class PathingPointMovement : MonoBehaviour
 {
     [Header("Pathing Points")]
+    public Transform player;
     public GameObject point1;
     public GameObject point2;
     public GameObject point3;
     public GameObject point4;
 
-    [Header("stats")]
+    [Header("Stats")]
     public float speed;
-    public float delay;
     public float distance;
     public bool movingOut;
 
-    private void Start()
-    {
-        //MoveOut();
-    }
 
     private void Update()
     {
-        while (movingOut)
-        {
-            point1.transform.position = Vector3.Lerp(point1.transform.position, new Vector3(0, distance, 0), Time.deltaTime * speed);
-            point2.transform.position = Vector3.Lerp(point2.transform.position, new Vector3(distance, 0, 0), Time.deltaTime * speed);
-            point3.transform.position = Vector3.Lerp(point3.transform.position, new Vector3(0, -distance, 0), Time.deltaTime * speed);
-            point4.transform.position = Vector3.Lerp(point4.transform.position, new Vector3(-distance, 0, 0), Time.deltaTime * speed);
+        Vector3 playerPosition = player.position; // Use the player's position as the reference
 
-            if (point1.transform.position.y >= distance)
+        if (movingOut)
+        {
+            // Move points away from the player based on the distance
+            point1.transform.position = Vector3.Lerp(point1.transform.position, playerPosition + new Vector3(0, -distance, 0), Time.deltaTime * speed);
+            point2.transform.position = Vector3.Lerp(point2.transform.position, playerPosition + new Vector3(-distance, 0, 0), Time.deltaTime * speed);
+            point3.transform.position = Vector3.Lerp(point3.transform.position, playerPosition + new Vector3(0, distance, 0), Time.deltaTime * speed);
+            point4.transform.position = Vector3.Lerp(point4.transform.position, playerPosition + new Vector3(distance, 0, 0), Time.deltaTime * speed);
+
+            // Check when points are far enough to stop moving out
+            if (Vector3.Distance(point1.transform.position, playerPosition + new Vector3(0, -distance, 0)) < 0.1f)
+            {
                 movingOut = false;
+                Debug.Log("Moving In");
+            }
         }
 
-        while (!movingOut)
+        else
         {
-            point1.transform.position = Vector3.Lerp(point1.transform.position, new Vector3(0, -distance, 0), Time.deltaTime * speed);
-            point2.transform.position = Vector3.Lerp(point2.transform.position, new Vector3(-distance, 0, 0), Time.deltaTime * speed);
-            point3.transform.position = Vector3.Lerp(point3.transform.position, new Vector3(0, distance, 0), Time.deltaTime * speed);
-            point4.transform.position = Vector3.Lerp(point4.transform.position, new Vector3(distance, 0, 0), Time.deltaTime * speed);
+            // Move points back toward the player based on the distance
+            point1.transform.position = Vector3.Lerp(point1.transform.position, playerPosition, Time.deltaTime * speed);
+            point2.transform.position = Vector3.Lerp(point2.transform.position, playerPosition, Time.deltaTime * speed);
+            point3.transform.position = Vector3.Lerp(point3.transform.position, playerPosition, Time.deltaTime * speed);
+            point4.transform.position = Vector3.Lerp(point4.transform.position, playerPosition, Time.deltaTime * speed);
 
-            if (point1.transform.position.y >= 0)
+            // Check when points are close enough to reverse direction
+            if (Vector3.Distance(point1.transform.position, playerPosition) < 0.01f)
+            {
                 movingOut = true;
+                Debug.Log("Moving Out");
+            }
         }
     }
-    void MoveOut()
-    {
-
-        Debug.Log("moved out");
-
-        for (int i = 0; i < 25; i++)
-        {
-            point1.transform.position = Vector3.Lerp(point1.transform.position, new Vector3(0, distance, 0), Time.deltaTime * speed);
-            point2.transform.position = Vector3.Lerp(point2.transform.position, new Vector3(distance, 0, 0), Time.deltaTime * speed);
-            point3.transform.position = Vector3.Lerp(point3.transform.position, new Vector3(0, -distance, 0), Time.deltaTime * speed);
-            point4.transform.position = Vector3.Lerp(point4.transform.position, new Vector3(-distance, 0, 0), Time.deltaTime * speed);
-    }
-
-    Invoke("MoveIn", delay);
-    }
-
-    void MoveIn()
-    {
-
-        Debug.Log("moved in");
-
-        for (int i = 0; i < 25; i++)
-        {
-            point1.transform.position = Vector3.Lerp(point1.transform.position, new Vector3(0, -distance, 0), Time.deltaTime * speed);
-            point2.transform.position = Vector3.Lerp(point2.transform.position, new Vector3(-distance, 0, 0), Time.deltaTime * speed);
-            point3.transform.position = Vector3.Lerp(point3.transform.position, new Vector3(0, distance, 0), Time.deltaTime * speed);
-            point4.transform.position = Vector3.Lerp(point4.transform.position, new Vector3(distance, 0, 0), Time.deltaTime * speed);
-        }
-
-        Invoke("MoveOut", delay);
-    }
-
-
 }
