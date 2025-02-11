0f83e96fe4cd9cea13a9bb41581cf9fa8e743d22
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-02-11 14:12:49 -0500

    Player now has a walk and idle animation, and can go between them

--- Assets/Scripts/PlayerSprite.cs
+++ Assets/Scripts/PlayerSprite.cs
@@ -8,24 +8,36 @@ public class PlayerSprite : MonoBehaviour
 {
     PlayerController controller;
     SpriteRenderer sprite;
+    Animator animator;
+
     void Start()
     {
         GameObject player = GameObject.Find("Player");
         controller = player.GetComponent<PlayerController>();
         
         sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+
+        
     }
 
 
     void Update()
     {
+        // Handle flipping the sprite
         if (controller.moveDirection == PlayerController.MoveDirection.DOWNLEFT || controller.moveDirection == PlayerController.MoveDirection.LEFT || controller.moveDirection == PlayerController.MoveDirection.UPLEFT)
-        {
             sprite.flipX = true;
-        }
+        
         else if (controller.moveDirection == PlayerController.MoveDirection.DOWNRIGHT || controller.moveDirection == PlayerController.MoveDirection.RIGHT || controller.moveDirection == PlayerController.MoveDirection.UPRIGHT)
-        {
             sprite.flipX = false;
-        }  
+        
+
+        //Handle walk animations
+        if (controller.moveDirection == PlayerController.MoveDirection.STATIC)
+            animator.SetBool("Static", true);
+                       
+        else
+            animator.SetBool("Static", false);
+        
     }
 }
