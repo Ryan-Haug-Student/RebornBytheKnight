86b5aadc964ed41931f5e7bdd0a667780d7fd89d
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-02-12 13:50:54 -0500

    Added a pause menu

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,20 +2,30 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    public PlayerController playerController;
-    public GameObject player;
-    public GameObject health;
+    PlayerController playerController;
+    GameObject player;
+    GameObject health;
+
+    GameObject InGameElements;
+    [SerializeField] GameObject pauseMenu;
+
+    public bool paused;
 
     void Start()
     {
         player = GameObject.Find("Player");
         playerController = player.GetComponent<PlayerController>();
+
         health = GameObject.Find("Health");
+        InGameElements = GameObject.Find("InGameElements");
     }
 
     void Update()
     {
-        HealthBar();
+        HealthBar(); //update the health bar pos
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Pause();
     }
 
     void HealthBar()
@@ -27,5 +37,29 @@ public class UI : MonoBehaviour
 
         // Update health bar position
         health.transform.localPosition = new Vector3(-0.21f * (1 - target), -0.02f, 0);
+    }
+
+    public void Pause()
+    {
+        if (!paused)
+        {
+            InGameElements.SetActive(false);
+            pauseMenu.SetActive(true);
+
+            Time.timeScale = 0;
+            paused = true;
+
+            Debug.Log("Paused");
+        }
+        else
+        {
+            InGameElements.SetActive(true);
+            pauseMenu.SetActive(false);
+
+            Time.timeScale = 1;
+            paused = false;
+
+            Debug.Log("unPaused");
+        }
     }
 }
