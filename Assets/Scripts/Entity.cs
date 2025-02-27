85aca695b141966352f93cb2bb786e1ef08ab69a
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-02-27 14:47:35 -0500

    restarting lmao

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    Rigidbody rb;
+    SpriteRenderer sr;
+
+    [Header("Health")]
+    public int health;
+    public int maxHealth;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        sr = GetComponent<SpriteRenderer>();
+
+        maxHealth = health;
+    }
+
+    protected virtual void Update()
+    {
+        SpriteHandling();
+    }
+
+    private void SpriteHandling()
+    {
+        if (rb.velocity.x > 0)
+            sr.flipX = false;
+        else if (rb.velocity.x < 0)
+            sr.flipX = true;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+            Die();
+    }
+
+    public virtual void Die() //virtual to be overriden by player to show stats and end run
+    {
+        print(gameObject + "Died");
+        Destroy(gameObject);
+    }
+}
