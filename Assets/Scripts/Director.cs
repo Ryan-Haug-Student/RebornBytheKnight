56764da4fcfb91c41abca38e0a5becc2bbec9c31
Ryan-Haug-Student <ryha601@student.oaisd.org>
2025-03-04 19:51:10 -0500

    started work on a director

---
+++ Assets/Scripts/Director.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour
+{
+    //credits to spawn
+    //reference to the current stage
+    //a bool to tell when the player can teleport / stage over
+
+    //a bool to tell when spawn
+
+    //array reference to the enemys
+    //array reference to cost : enemy[0] = cost[0]
+
+    //create a random int for, how many enemies, which enemies, and time to next wave
+}
